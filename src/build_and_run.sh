035b78fa90d59ab619f3df109c8f22c782bdaadb
Samuel Messing <samuel.messing@gmail.com>
2018-08-26 15:49:33 +0000

    starting virtualenv for project.

--- src/build_and_run.sh
+++ src/build_and_run.sh
@@ -8,11 +8,24 @@ set -ex
 # TODO - kill existing server first.
 
 readonly ROOT="src"
+readonly VIRTUALENV_ROOT="otto-env"
 
-# Initial cleaning of old artifacts.
+# CLEANING ===========================================
 rm -f "${ROOT}/genfiles/*"
 
-# Building protos.
+# VIRTUALENV =========================================
+if [ ! -d "${VIRTUALENV_ROOT}" ]; then
+	virtualenv otto-env
+	# activate is idempotent
+	. otto-env/bin/activate
+	pip install -r "${ROOT}/requirements.txt"
+fi
+
+# activate is idempotent
+. otto-env/bin/activate
+
+
+# BUILDING PROTOS ====================================
 protoc -I="${ROOT}/proto/" \
 	--python_out="${ROOT}/genfiles/" \
 	${ROOT}/proto/*

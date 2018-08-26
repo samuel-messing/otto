e973fa091871cd028ca275904d95fda4e579917d
Samuel Messing <samuel.messing@gmail.com>
2018-08-26 15:29:24 +0000

    building initial proto for configuring pumps.

--- src/build_and_run.sh
+++ src/build_and_run.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+set -ex
 
 # Builds entire project and runs the server.
 
-# TODO - build protos.
+# TODO - check that we're running in top-level dir.
 # TODO - run server.
 # TODO - kill existing server first.
+
+readonly ROOT="src"
+
+# Initial cleaning of old artifacts.
+rm -f "${ROOT}/genfiles/*"
+
+# Building protos.
+protoc -I="${ROOT}/proto/" \
+	--python_out="${ROOT}/genfiles/" \
+	${ROOT}/proto/*

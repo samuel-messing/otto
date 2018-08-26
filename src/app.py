5f20c3b8b64dfdd69f32758534703a2b8bafb2b1
Samuel Messing <samuel.messing@gmail.com>
2018-08-26 19:31:06 +0000

    pumps now have dynamic state, can use index.html to change state from true to false.

--- src/app.py
+++ src/app.py
@@ -1,33 +1,42 @@
-from flask import Flask, render_template
-from google.protobuf import text_format
+from flask import Flask, redirect, render_template, send_from_directory
 from optparse import OptionParser
 from pump import Pump
-from pump_pb2 import Pumps
 import sys
 
 APP = Flask(__name__)
-PUMPS = Pumps()
-
-@APP.route('/v1/pump/<pump_no>/<action>')
-def pump(pump_no, action):
-  print pump_no + " action: " + action
-  return pump_no
+# A dictionary of pump_name: Pump
+PUMPS = None
 
 @APP.route('/')
 def index():
-  return render_template('index.html', pumps = PUMPS)
+  return render_template('index.html', pumps = Pump.to_proto(PUMPS.values()))
+
+@APP.route('/js/<path>/')
+def js(path):
+  return send_from_directory('js', path)
+
+@APP.route('/css/<path>/')
+def css(path):
+  return send_from_directory('css', path)
+
+@APP.route('/v1/pump/<name>/<action>')
+def pump(name, action):
+  pump = PUMPS[name]
+  if pump is not None:
+    pump.on() if action == 'on' else pump.off()
+  else:
+    print "ERROR: No pump with name " + name + " exists."
+  return redirect('/', code=302)
 
 if __name__ == "__main__":
   parser = OptionParser()
   parser.add_option("-c", "--config_file", dest="config_file",
                     help="path to config_file for pumps", metavar="FILE")
   (options, args) = parser.parse_args()
-  with open(options.config_file, 'r') as f:
-    content = f.read()
-    text_format.Merge(content, PUMPS)
+  PUMPS = Pump.load_from_file(options.config_file)
 
-  if len(PUMPS.pumps) == 0:
-    print "Failed to parse config file (empty?)"
+  if PUMPS is None or len(PUMPS) == 0:
+    print "Failed to parse config file: " + options.config_file + " (empty?)"
     sys.exit(1)
 
   print "Running with config:"
--- src/pump.py
+++ src/pump.py
@@ -1,9 +1,44 @@
-import pump_pb2
+from google.protobuf import text_format
+from pump_pb2 import Pumps
 
 class Pump(object):
   OFF = 1
   ON = 2
 
-  def __init__(self, id):
-    self.id = id
-    self.state = Pump.OFF
+  def __init__(self, proto_pump):
+    self.name = proto_pump.name
+    self.is_running = proto_pump.is_running
+    self.gpio_pin = proto_pump.gpio_pin_number
+
+  def __repr__(self):
+    return self.name + " - (GPIO=" + str(self.gpio_pin) + ")"
+
+  def on(self):
+    self.is_running = True
+
+  def off(self):
+    self.is_running = False
+
+  @staticmethod
+  def load_from_file(filename):
+    pumps = Pumps()
+    with open(filename, 'r') as f:
+      content = f.read()
+      text_format.Merge(content, pumps)
+      return Pump.load_from_proto(pumps)
+    return None
+
+  @staticmethod
+  def load_from_proto(proto_pumps):
+    return { proto_pump.name: Pump(proto_pump) for proto_pump in proto_pumps.pumps }
+
+  @staticmethod
+  def to_proto(pumps):
+    '''Converts a list of pump objects into a Pumps protobuf.'''
+    pumps_pb = Pumps()
+    for pump in pumps:
+      pump_pb = pumps_pb.pumps.add()
+      pump_pb.name = pump.name
+      pump_pb.gpio_pin_number = pump.gpio_pin
+      pump_pb.is_running = pump.is_running
+    return pumps_pb
